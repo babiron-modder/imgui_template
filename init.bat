d326b635a50993a3b1efb47514fca73b06bc02f5
babiron! <babi@example.com>
2021-06-17 22:45:13 +0900

    change init.bat

--- init.bat
+++ init.bat
@@ -1,9 +1,15 @@
 
-git submodule init
-git submodule update
+rm .git
+git init
+rd gl3w /S /Q
+rd imgui /S /Q
+rd glm /S /Q
+git init
 git submodule add https://github.com/ocornut/imgui.git imgui
 git submodule add https://github.com/skaslev/gl3w.git gl3w
 git submodule add https://github.com/g-truc/glm.git glm
+git submodule init
+git submodule update
 cd gl3w
 python gl3w_gen.py
 cd ../
